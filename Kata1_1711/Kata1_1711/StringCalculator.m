bcc0828f5120b1e941b7de57261e7177369ba9e4
lithewall <truonghm@WifiFree.local>
2013-11-18 14:05:34 +0700

    Kata1_1711 1.1: red

--- Kata1_1711/Kata1_1711/StringCalculator.m
+++ Kata1_1711/Kata1_1711/StringCalculator.m
@@ -10,4 +10,11 @@
 
 @implementation StringCalculator
 
+-(NSInteger)add:(NSString*)str{
+    NSInteger result;
+
+    
+    return result;
+}
+
 @end
--- Kata1_1711/Kata1_1711Tests/StringCalculatorSpec.m
+++ Kata1_1711/Kata1_1711Tests/StringCalculatorSpec.m
@@ -7,7 +7,39 @@
 #import "StringCalculator.h"
 
 SPEC_BEGIN(StringCalculatorSpec)
+__block NSString *input;
+__block StringCalculator *str;
 describe(@"StringCalculator test", ^{
+    afterAll(^{
+        str = [[StringCalculator alloc] init];
+    });
+    
+    beforeAll(^{
+        str = nil;
+    });
+    
+    it(@"Test voi ky tu rong", ^{
+        input = @"";
         
+        NSInteger outPut = [str add:input];
+        
+        [[theValue(outPut) should] equal:theValue(0)];
+    });
+    
+    it(@"Test voi mot so: 1", ^{
+        input = @"1";
+        
+        NSInteger outPut = [str add:input];
+        
+        [[theValue(outPut) should] equal:theValue(1)];
+    });
+    
+    it(@"Test voi hai so: 12,3", ^{
+        input = @"12,3";
+        
+        NSInteger outPut = [str add:input];
+        
+        [[theValue(outPut) should] equal:theValue(15)];
+    });
 });
 SPEC_END
