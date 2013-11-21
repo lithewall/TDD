0d4b0942363650931af5ae378f6ad8571afb3115
lithewall <truonghm@WifiFree.local>
2013-11-21 16:49:45 +0700

    Kata1_2111 1.1 blue

--- Kata1_2111/Kata1_2111/StringCalculator.m
+++ Kata1_2111/Kata1_2111/StringCalculator.m
@@ -11,9 +11,34 @@
 @implementation StringCalculator
 
 -(NSInteger)add:(NSString*)str{
-    NSInteger resuft;
+    NSInteger resuft =0;
+    NSMutableArray *listNum = [NSMutableArray array];
+    
+    listNum = [self getTextNoHaveDelimiterFromStr:str];
+    
+    for (NSString *oneNum in listNum) {
+        resuft += oneNum.integerValue;
+    }
     
     return resuft;
+}
+
+-(NSMutableArray*)getTextNoHaveDelimiterFromStr:(NSString*)str{
+    NSMutableArray *result = [NSMutableArray array];
+    
+    NSRange range = [str rangeOfString:@","];
+    
+    if (range.length) {
+        NSString *firstStr = [str substringWithRange:NSMakeRange(0,range.location)];
+        NSString *secondStr = [str substringWithRange:NSMakeRange(range.location+1, str.length-range.length-range.location)];
+        
+        [result addObject:firstStr];
+        [result addObject:secondStr];
+    }else{
+        [result addObject:str];
+    }
+    
+    return result;
 }
 
 @end
